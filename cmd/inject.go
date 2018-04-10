79162795182b0bf79f31401dc501ff1a04ed04e9
Kevin LARQUEMIN <kevin.larquemin@continental-corporation.com>
2018-04-10 22:49:54 +0200

    cleaning rootcmd and licensing

--- cmd/inject.go
+++ cmd/inject.go
@@ -18,10 +18,39 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-package main
+package cmd
 
-import "github.com/jsenon/kubextractor/cmd"
+import (
+	"fmt"
 
-func main() {
-	cmd.Execute()
+	"github.com/spf13/cobra"
+)
+
+// injectCmd represents the inject command
+var injectCmd = &cobra.Command{
+	Use:   "inject",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("inject called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(injectCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// injectCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// injectCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/root.go
+++ cmd/root.go
@@ -1,205 +1,50 @@
+// Copyright © 2018 Julien SENON <julien.senon@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 package cmd
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"os"
-	"os/exec"
-	"os/user"
 
+	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
-// Config Struct of JSON Kubernetes Config File
-type Config struct {
-	Kind        string `json:"kind"`
-	APIVersion  string `json:"apiVersion"`
-	Preferences struct {
-	} `json:"preferences"`
-	Clusters []struct {
-		Name    string `json:"name"`
-		Cluster struct {
-			Server                string `json:"server"`
-			InsecureSkipTLSVerify bool   `json:"insecure-skip-tls-verify"`
-		} `json:"cluster"`
-	} `json:"clusters"`
-	Users []struct {
-		Name string `json:"name"`
-		User struct {
-			ClientCertificateData string `json:"client-certificate-data"`
-			ClientKeyData         string `json:"client-key-data"`
-			UserName              string `json:"username"`
-			Password              string `json:"password"`
-		} `json:"user"`
-	} `json:"users"`
-	Contexts []struct {
-		Name    string `json:"name"`
-		Context struct {
-			Cluster string `json:"cluster"`
-			User    string `json:"user"`
-		} `json:"context"`
-	} `json:"contexts"`
-	CurrentContext string `json:"current-context"`
-}
-
-var jsonfile string
-var context string
 var cfgFile string
-var output string
 
+// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "kubextractor",
 	Short: "Extract k8s context from global config file",
 	Long: `Extract kubernetes context ie. configuration user and endpoint.
-				  Complete documentation is available at https://github.com/jsenon/kubextractor
-				  After export Use kubectl config use-context YOURCONTEXT --kubeconfig output.json to use it`,
-	// Args: cobra.MinimumNArgs(1),
-
-	Run: func(cmd *cobra.Command, args []string) {
-
-		defaultfilejson := "/.kube/config.json"
-		defaultfile := "/.kube/config"
-
-		tempfile := ".convert.json"
-
-		usr, err := user.Current()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		cmdName := "kubectl"
-		cmdArgs := []string{"config", "view", "-o", "json", "--raw", "--kubeconfig", usr.HomeDir + defaultfile}
-
-		// If no value for config k8s file, use default config but we need to convert to json
-		if cfgFile == "" && jsonfile == "" {
-
-			cfgFile = usr.HomeDir + defaultfile
-			out, erro := exec.Command(cmdName, cmdArgs...).Output() // #nosec
-			if erro != nil {
-				log.Fatal(erro)
-			}
-
-			err = ioutil.WriteFile(tempfile, out, 0644)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			jsonfile = tempfile
-		}
-
-		// If value for config k8s but no json we need to generate a json output
-		if cfgFile != "" && jsonfile == "" {
-
-			cmdArgs := []string{"config", "view", "-o", "json", "--raw", "--kubeconfig", cfgFile}
-
-			cfgFile = usr.HomeDir + defaultfile
-			out, erro := exec.Command(cmdName, cmdArgs...).Output() // #nosecx
-			if erro != nil {
-				log.Fatal(erro)
-			}
-
-			err = ioutil.WriteFile(tempfile, out, 0644)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			jsonfile = tempfile
-
-		}
-
-		// Used default value for json config file
-		// Exit if doesn't exist
-		if jsonfile == "" {
-			jsonfile = usr.HomeDir + defaultfilejson
-
-		}
-
-		file, err := os.Open(jsonfile)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		defer file.Close() // nolint: errcheck
-
-		b, err := ioutil.ReadAll(file)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		res := &Config{}
-		var configoutput Config
-
-		err = json.Unmarshal([]byte(string(b)), &res)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		configoutput.APIVersion = res.APIVersion
-		configoutput.Kind = res.Kind
-
-		// Loop over Clusters matching with context asked
-		for _, coutput := range res.Clusters {
-
-			if coutput.Name == context {
-				configoutput.Clusters = append(configoutput.Clusters, coutput)
-
-			}
-
-		}
-
-		// Loop over Users matching with contexy asked
-		for _, coutput := range res.Users {
-
-			if coutput.Name == context {
-				configoutput.Users = append(configoutput.Users, coutput)
-			}
-
-		}
-
-		// Loop over Contexts matching with contexy asked
-		for _, coutput := range res.Contexts {
-
-			if coutput.Name == context {
-				configoutput.Contexts = append(configoutput.Contexts, coutput)
-			}
-
-		}
-
-		// Output to console
-		if output == "" {
-			body, erro := json.MarshalIndent(configoutput, "", "   ")
-			if erro != nil {
-				log.Fatal(erro)
-			}
-			fmt.Println(string(body))
-		} else {
-
-			//Write to output file specified in args
-			body, erro := json.MarshalIndent(configoutput, "", "   ")
-			if erro != nil {
-				log.Fatal(erro)
-			}
-
-			err = ioutil.WriteFile(output, body, 0644)
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Println("Exported to:", output)
-
-		}
-
-		// Delete temporary file
-		err = os.Remove(tempfile)
-		if err != nil {
-			log.Fatal(err)
-		}
-	},
+Complete documentation is available at https://github.com/jsenon/kubextractor
+After export Use kubectl config use-context YOURCONTEXT --kubeconfig output.json to use it`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute Viper Command
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
@@ -207,34 +52,41 @@ func Execute() {
 	}
 }
 
-// Func to init Cobra Flag and bind flag
 func init() {
+	cobra.OnInitialize(initConfig)
 
-	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "k8s config file default ($HOME/.kube/config)")
-	rootCmd.PersistentFlags().StringVarP(&jsonfile, "configjson", "j", "", "k8s config file JSON default ($HOME/.kube/config.json)")
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.kubecm.yaml)")
 
-	rootCmd.PersistentFlags().StringVarP(&context, "context", "e", "", "MANDATORY: Name of  context to extract")
-	rootCmd.PersistentFlags().StringVarP(&output, "output", "o", "", "Name of output file")
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
 
-	err := rootCmd.MarkPersistentFlagRequired("context")
-	if err != nil {
-		log.Fatal(err)
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".kubecm" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".kubecm")
 	}
 
-	err = viper.BindPFlag("jsonfile", rootCmd.PersistentFlags().Lookup("jsonfile"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = viper.BindPFlag("context", rootCmd.PersistentFlags().Lookup("context"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = viper.BindPFlag("output", rootCmd.PersistentFlags().Lookup("output"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = viper.BindPFlag("cfgFile", rootCmd.PersistentFlags().Lookup("cfgFile"))
-	if err != nil {
-		log.Fatal(err)
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
 }
